fb7acc4771e0d547eb8a8432e7ea8906125f3905
oskarnrk <o.zambotti@fbk.eu>
2016-07-13 17:56:37 +0200

    Working on events list and filters

--- explorer-mobile/www/js/controllers/EventsCtrl.js
+++ explorer-mobile/www/js/controllers/EventsCtrl.js
@@ -1,10 +1,39 @@
 angular.module('explorer.controllers.events', [])
 
-.controller('EventsCtrl', function ($scope, $state, $stateParams, Config, DbSrv) {
+.controller('EventsCtrl', function ($scope, $state, $stateParams, $ionicScrollDelegate, $ionicPopup, Config, DbSrv) {
   $scope.events = null;
 
   var category = !!$stateParams['category'] ? $stateParams['category'] : 'All';
 
   // Cultura, Sociale, Sport, Other, All
   $scope.events = DbSrv.getDbObject()[category];
+
+  $scope.filter = {
+    open: false,
+    toggle: function () {
+      this.open = !this.open;
+      if (this.open) {
+        $ionicScrollDelegate.resize();
+      }
+    },
+    filterBy: function (selection) {
+      this.selected = selection;
+      // you can use a watch to do the filtering
+      $scope.updateList(this.selected);
+      this.toggle();
+    },
+    options: [],
+    selected: null
+  };
+
+  $scope.filter.options = DbSrv.getCategories();
+
+  //$scope.$watch('filter.selected', function (newSelection, oldSelection) {});
+
+  /* Date filter popup */
+  var dateFilterPopup = $ionicPopup.show({
+    templateUrl: 'templates/popup_eventsdatefilter.html',
+    title: 'TEST',
+    scope: $scope
+  });
 });
